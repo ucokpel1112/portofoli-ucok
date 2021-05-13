d5531a3582dadac590de84f2812f05b1fe052b3d
Kristopel Lumbantoruan <49096032+ucokpel1112@users.noreply.github.com>
2021-05-13 14:02:44 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -5,7 +5,8 @@ Date : 7 April 2021
 */
 document.addEventListener('contextmenu', event => event.preventDefault());
 
-var domain = "https://ucokpel1112.github.io/portfolio/";
+var domain = "https://ucokpel1112.github.io/portofolio/";
+
 var c = document.querySelector('p[class="mt-2"]').textContent;
 if(c != "\xa9\x20\x46\x69\x6b\x72\x69\x20\x4d\x61\x75\x6c\x61\x6e\x61\x20\x28\x4b\x6f\x46\x69\x44\x65\x76\x29\x20\x7c\x20\x4d\x61\x64\x65\x20\u2764\ufe0f"){
     $("#content").html('\x48\x61\x72\x67\x61\x69\x20\x43\x6f\x70\x79\x72\x69\x67\x68\x74\x20\ud83d\ude1d');
